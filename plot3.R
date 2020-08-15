a137d24409e53581e887eeb3e85b5d13e597e402
georgekevillil <georgekevillil@gmail.com>
2020-08-15 22:35:50 +0530

    plot3

---
+++ plot3.R
@@ -0,0 +1,20 @@
+setwd("C:/Users/George Jose/Desktop/R projects/coursera/Exploratory data analysis/week 1/data/ExData_Plotting1")
+data <- read.csv2(file = "household_power_consumption.txt", na.strings = "?")
+
+##Subset the required dates
+subdata <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+
+#duplicating subdata
+dup <- subdata
+
+subdata$DateTime <- paste(subdata$Date,subdata$Time)
+subdata$DateTime <- strptime(subdata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+subdata$Sub_metering_1 <- as.numeric(subdata$Sub_metering_1)
+subdata$Sub_metering_2 <- as.numeric(subdata$Sub_metering_2)
+subdata$Sub_metering_3 <- as.numeric(subdata$Sub_metering_3)
+
+plot(y = subdata$Sub_metering_1, x = subdata$DateTime, type = "l", ylab = "Energy submetering", xlab = "" )
+lines(x = subdata$DateTime,subdata$Sub_metering_2,col = "red")
+lines(y = subdata$Sub_metering_3, x = subdata$DateTime, col = "blue")
+dev.copy(png, file ="plot3.png")
+dev.off()
